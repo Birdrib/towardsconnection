e2d735b12361cc9134ceef32149536ef68a5df2f
Birdrib <afrog14@gmail.com>
2021-03-06 09:40:35 -0600

    Update videoNav.js

--- videoNav.js
+++ videoNav.js
@@ -1,25 +1,30 @@
 var orig = document.getElementById("original");
 var squiggle = document.getElementById("squiggle");
 var circleGIF = document.getElementById("circleGIF");
+
 var button = document.getElementById("beginbutton");
 
+var vidid = ["squiggle", "original"]
+var gifid = ["circleGIF", "circleGIF"]
       
-function playVid() { 
-  button.style.display="none";
-  orig.play();
+function buttonPress() { 
+	button.style.display="none";
+	button.classList.toggle("lightbutton");
+	button.classList.toggle("darkbutton");
+	squiggle.play();
 }  
       
 function doSomething() {
-  shuffleDown();
-  squiggle.play();
+	shuffleDown();
+	squiggle.play();
 }
       
 function shuffleDown() {
-  orig.style.zIndex="-1";
-  circleGIF.style.zIndex="-1";
+	orig.style.zIndex="-1";
+	circleGIF.style.zIndex="-1";
 }
       
 function idle() {
-  circleGIF.style.zIndex="2";
-  button.style.display="block";
+	circleGIF.style.zIndex="2";
+	button.style.display="block";
 }
