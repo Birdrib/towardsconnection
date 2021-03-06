3efe3e874127ef6fe278e0b150226ea8eb0f291b
Birdrib <afrog14@gmail.com>
2021-03-06 10:05:23 -0600

    Update videoNav.js

--- videoNav.js
+++ videoNav.js
@@ -7,11 +7,12 @@ var button = document.getElementById("beginbutton");
 var vidid = ["squiggle", "original"];
 var gifid = ["circleGIF", "circleGIF"];
 
-var index = 0;
+var index = -1;
       
 function buttonPress() { 
 	index = index + 1;
 	//button.style.display="none";
+	button.value="I've been clicked"
 	button.classList.toggle("lightbutton");
 	button.classList.toggle("darkbutton");
 	advanceVideo();
