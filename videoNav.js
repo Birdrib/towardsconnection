0f9e06dbf96dd68953ed22e22385da9a1c5c4b12
Birdrib <afrog14@gmail.com>
2021-03-19 02:27:29 -0500

    Update videoNav.js

--- videoNav.js
+++ videoNav.js
@@ -1,7 +1,7 @@
 var bbutton = document.getElementById("beginbutton");
 
-var vidid = ["loopback", "l_ind", "d_ind", "l_coord", "d_coord", "l_org", "d_org", "l_reor", "d_reor", "l_con", "end"]; //first video is transition from end back to beginning
-var gifid = ["startGIF", "wait_l_ind", "wait_d_ind", "wait_l_coord", "wait_d_coord", "wait_l_org", "wait_d_org", "wait_l_reor", "wait_d_reor", "wait_l_con", "wait_end"]; //first gif is start gif
+var vidid = ["loopback", "l_ind", "d_ind", "l_coord", "d_coord", "l_org", "d_org", "l_reor", "d_reor", "l_con", "d_con", "end"]; //first video is transition from end back to beginning
+var gifid = ["startGIF", "wait_l_ind", "wait_d_ind", "wait_l_coord", "wait_d_coord", "wait_l_org", "wait_d_org", "wait_l_reor", "wait_d_reor", "wait_l_con", "wait_d_con", "wait_end"]; //first gif is start gif
 
 var index = 0;
 var end = vidid.length;
